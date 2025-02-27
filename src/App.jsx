25cddd745b336ece5d1285c485a13ebf74f97576
srii01 <sripriyahg_ece@ksit.edu.in>
2025-02-27 12:48:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
-import { useState } from "react";
+import React from "react";
 
-function TodoList() {
-  const [tasks, setTasks] = useState([]);
-  const [input, setInput] = useState("");
-
-  const addTask = () => {
-    if (input.trim() !== "") {
-      setTasks([...tasks, input]);
-      setInput("");
-    }
-  };
-
+function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <h1>My To-Do List</h1>
-      {/* Label is correctly associated with input using htmlFor */}
-      <label htmlFor="taskInput">Enter Task:</label>
- <input
-  type="text"
-  id="taskInput"  // Unique ID
-  name="task"     // Name attribute added
-  value={input}
-  onChange={(e) => setInput(e.target.value)}
-  placeholder="Add a task..."
-  autoComplete="off"  // Optional, prevents unwanted autofill
-/>
-
-      <button onClick={addTask}>Add</button>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
+    <div>
+      <h1 style={{ color: "red" }}>React is Rendering!</h1>
     </div>
   );
 }
 
-export default TodoList;
+export default App;
+
 
 
