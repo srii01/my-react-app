73ba2acf81fa9dc18b703247d1cef5a161757e93
srii01 <sripriyahg_ece@ksit.edu.in>
2025-02-27 11:45:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import TodoList from './TodoList';
+import { useState } from 'react';
 
-function App() {
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTask = () => {
+    if (input.trim() !== '') {
+      setTasks([...tasks, input]);
+      setInput('');
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>My To-Do List</h1>
-      <TodoList />
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a task..."
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default TodoList;
