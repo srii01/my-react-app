f1d4698703e9278360e330065f09cad4c3c5a76c
srii01 <sripriyahg_ece@ksit.edu.in>
2025-02-27 11:47:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-react-app/',  // Your GitHub repository name
+  base: 'https://github.com/srii01/my-react-app',  // Your GitHub repository name
   plugins: [react()]
 });
