411f0c272116d82e4468658275e3be606e284dcd
srii01 <sripriyahg_ece@ksit.edu.in>
2025-02-27 11:33:20 +0530

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/my-react-app/',  // Your GitHub repository name
-  plugins: [react()]
-});
